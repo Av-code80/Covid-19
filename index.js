2cea98f0837347ce5347b1122f99aa7b853737fe
avehghasemian <av.ghasemian@gmail.com>
2021-03-01 00:23:15 +0100

    btnJS

---
+++ index.js
@@ -0,0 +1,7 @@
+//Creation an information button in the statistic board.
+function btnFunction() {
+    var p = document.getElementById("paragraph");
+    p.style.display = "block";   
+  };
+
+    
