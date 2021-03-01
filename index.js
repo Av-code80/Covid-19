3a7d9779c9ec1c3100f1d7ac0e2717a5446887a5
avehghasemian <av.ghasemian@gmail.com>
2021-03-01 18:32:45 +0100

    btnJS

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 //Creation an information button in the statistic board.
-function btnFunction() {
+/*function btnFunction() {
     var p = document.getElementById("paragraph");
     p.style.display = "block";   
   };
+*/
 
-    
+/*
+function btnFunction() {
+const btn = document.getElementById("button");
+const createBtn = document.createElement(button)
+createBtn.innerHTML("information");
+createBtn.classList.add("classBtn");
+btn.appendChild("IRAAAAAN");
+}
+*/
+
+function myFunction() {
+  var x = document.createElement("P");
+  var t = document.createTextNode("Iran	60570	80880	5999	9000	Clusters of cases");
+  x.appendChild(t);
+  document.body.appendChild(x); 
+}
