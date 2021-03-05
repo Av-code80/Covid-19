5d3c8263a28f2217eee6dbce6452a17d7fdc2c0d
avehghasemian <av.ghasemian@gmail.com>
2021-03-05 19:22:24 +0100

    fetchAPI

---
+++ fetch.js
@@ -0,0 +1,21 @@
+
+// import { MY_API_KEY } from './config.js';
+
+fetch("https://covid-19-data.p.rapidapi.com/report/totals?date=2020-07-21", {
+     "method": "GET",
+    "headers": {
+        "x-rapidapi-key":  "314cde1626msh0c36ecd28436040p1c58d5jsnd0c56333634c",
+        "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        console.log(response.total);
+    })
+    .catch(err => {
+        console.error(err); 
+    });
+
+
+    
