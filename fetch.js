066feeafdf1d3bc0e215c5c8fff1209746066b29
avehghasemian <av.ghasemian@gmail.com>
2021-05-08 02:00:24 +0200

    optimised codes

--- fetch.js
+++ fetch.js
@@ -22,44 +22,7 @@ fetch("https://covid-19-data.p.rapidapi.com/report/totals?date=2020-07-21", {
 
 
 
-    
-    // fetch('/article/promise-chaining/user.json')
-    // .then(response => response.json())
-    // .then(user => fetch(`https://api.github.com/users/${user.name}`))  ??
-    // .then(response => response.json())
-    // .then(githubUser => new Promise(function(resolve, reject) { // (*)   .then handler //??
-    //   let img = document.createElement('img');
-    //   img.src = githubUser.avatar_url;
-    //   img.className = "promise-avatar-example";
-    //   document.body.append(img);
-  
-    //   setTimeout(() => {
-    //     img.remove();
-    //     resolve(githubUser); // (**)
-    //   }, 3000);
-    // }))
-    // // triggers after 3 seconds
-    // .then(githubUser => alert(`Finished showing ${githubUser.name}`));
 
 
 
 
-    
-
-    // function Counter() {      // whu uppercase (Counter)??
-    //     let count = 0;
-        
-    //     this.up = function() {
-    //       return ++count;
-    //     };
-        
-    //     this.down = function() {
-    //       return --count;
-    //     };
-    //   }
-      
-    //    let counter = new Counter;      // why ??
-      
-    //   alert(counter.up());
-    //   alert(counter.up());
-    //   alert(counter.down());
--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-// let age = prompt("What is your age?", 18);      Arrow functions can be used in the same way as Function Expressions.
-// let welcome = (age < 18) ?
-  // () => alert('Hello') :
-  // () => alert("Greetings!");
-
-
-// welcome();
-
-//Creation an information button in the statistic board.
-/*function btnFunction() {
-    var p = document.getElementById("paragraph");
-    p.style.display = "block";   
-  };
-*/
-
-/*
-function btnFunction() {
-const btn = document.getElementById("button");
-const createBtn = document.createElement(button)
-createBtn.innerHTML("information");
-createBtn.classList.add("classBtn");
-btn.appendChild("IRAAAAAN");
-}
-*/
-/*
-function myFunction() {
-  var x = document.createElement("P");
-  var t = document.createTextNode("Iran	60570	80880	5999	9000	Clusters of cases");
-  x.appendChild(t);
-  document.body.appendChild(x); 
-}*/
-
 
 function colGenerator(col) {                      //?? col ?
   let td = document.createElement("TD");
